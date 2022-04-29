d38067e6d60210d5e739a8aae354a93a4e248728
Jan Vojíř <jenvojir@gmail.com>
2022-04-29 10:58:32 +0200

    1st page don - kontakt in progress

---
+++ individual work/javascript/index.js
@@ -0,0 +1,28 @@
+alert ("hello world");
+console.log("hello js");
+
+const cars = ["audi", "tesla", "vw", "skoda", "porsche"]
+
+console.log(cars[2]);
+
+
+                                                                            
+
+cars.forEach((car) => {
+    console.log("idk: ");
+    console.log(car);
+});
+
+function clickMe() {
+    let x = 7;
+    let y = 5;
+
+    if (x+y > 10)
+    {
+        console.log("penis")
+    }
+    else {
+        console.log("nopenis")
+    }
+    console.log("button");
+}
