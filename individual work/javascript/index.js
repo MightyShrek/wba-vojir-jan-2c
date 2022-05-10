10247d9db81dfe150441163dfdffc4bfa85b5f58
Jan Vojíř <jenvojir@gmail.com>
2022-05-10 13:10:02 +0200

    update

--- individual work/javascript/index.js
+++ individual work/javascript/index.js
@@ -1,28 +1,9 @@
 alert ("hello world");
 console.log("hello js");
 
-const cars = ["audi", "tesla", "vw", "skoda", "porsche"]
-
-console.log(cars[2]);
-
-
-                                                                            
-
-cars.forEach((car) => {
-    console.log("idk: ");
-    console.log(car);
-});
 
 function clickMe() {
-    let x = 7;
-    let y = 5;
-
-    if (x+y > 10)
-    {
-        console.log("penis")
-    }
-    else {
-        console.log("nopenis")
-    }
+    let x = document.getElementById("text");
+    x.classList.toggle("hid");
     console.log("button");
 }
